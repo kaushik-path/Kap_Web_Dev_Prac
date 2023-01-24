f25b3c4e8657e9ead1f7c4170ec55d23fbc47b49
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2023-01-24 21:51:04 +0530

    Datatypes

---
+++ JavaScript_Prac/002_Datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+console.log("Hitesh") // Readability is a first priority
+
+
+let name = "hitesh" // String
+let age = 18 // Int
+let isLoggedIn = false //Boolen
+let state; // Undefined
+
+// NOTE ---
+    // Primitive Data types
+// number => 2 to power 53
+// bigint => For large the number
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+    // non Primitive Datatypes
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object

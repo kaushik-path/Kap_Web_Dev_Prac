c0ce7502b581d0045e32cce1ce4671fb848a2f59
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2023-01-24 21:08:23 +0530

    Starting CSS

---
+++ JavaScript_Prac/001_Variable.js
@@ -0,0 +1,14 @@
+const customerId = 123
+let customerEmail = "asd@qwe.in"
+var customerPassword = "kaka"
+customerCity = "Jalandhar"
+
+console.log( customerId );
+
+customerEmail = "ajka@ak.om"
+customerPassword = "21356"
+customerCity = "Delhi"
+let customerState ;
+
+
+console.table([customerEmail,customerId,customerPassword,customerCity, customerState]);

fd7f86f0a3c1b888e80b2131842677297913ba41
kaushik-path <85818550+kaushik-path@users.noreply.github.com>
2023-08-08 11:20:50 +0530

    note

--- JavaScript_Prac/002_Datatypes.js
+++ JavaScript_Prac/002_Datatypes.js
@@ -1,4 +1,5 @@
 "use strict"; // treat all JS code as newer version
+
 // alert( 3 + 3) // we are using nodejs, not browser
 
 console.log(3 
@@ -13,6 +14,8 @@ let isLoggedIn = false //Boolen
 let state; // Undefined
 
 // NOTE ---
+    // In JS we do not need to define the datatypes link in c++ or java int=9, int a = 889.
+    // we just simply define as let name ="aa", let year = 2023. and yes strings are always written in the "".
     // Primitive Data types
 // number => 2 to power 53
 // bigint => For large the number
